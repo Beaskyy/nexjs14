c6cb9274296247e362eca75606b29e2b69e2b1c4
Beaskyy <afeezakintobi@nqb8.co>
2023-12-11 21:57:36 +0100

    layouts

---
+++ src/components/header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header
+      style={{
+        padding: "24px",
+        backgroundColor: "rebeccapurple",
+        marginBottom: "30px",
+      }}
+    >
+      Header
+    </header>
+  );
+};
+
+export default Header;
