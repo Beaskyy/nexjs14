121d19173d81ec7ec1738485173278f304694b88
Beaskyy <afeezakintobi@nqb8.co>
2023-12-21 22:59:02 +0100

    add tailwindcss to the project

---
+++ src/app/bsk/page.tsx
@@ -0,0 +1,22 @@
+
+
+const Bsk = () => {
+  return (
+    <main className="w-full h-full bg-sky-500">
+      <div>
+        <div className="grid grid-cols-2">
+          <div>
+            <h1 className="text-2xl underline">
+              This is the left side of the grid
+            </h1>
+          </div>
+          <div>
+            <h1>This is the left side of the grid</h1>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Bsk;
